2592f2493d1a8dc1b092e163e99e748e095b8ed5
ia23a-perril <perril@bzz.ch>
2024-02-06 16:19:09 +0100

    static

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route('/about')
 def about() -> str:
     app.logger.info("Displaying about us page")
     return render_template("about.html")
+
 
 @app.route('/login')
 def login_home() -> str:
     app.logger.info("Rendering login form")
     return render_template("profile/index.html")
 
-#@app.route("/submit", methods=["POST"])
-#def login_submit() -> str:
 
+# @app.route("/submit", methods=["POST"])
+# def login_submit() -> str:
 
 
 if __name__ == '__main__':
