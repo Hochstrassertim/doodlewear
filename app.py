c9ad6cfeaddfdc1bd7979d97b0c992be482a290d
Tim Hochstrasser <hochstrassert@bzz.ch>
2024-02-06 10:14:52 +0100

    Working on the login form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route('/login')
+def login_home() -> str:
+    app.logger.info("Rendering login form")
+    return render_template("profile/index.html")
 
 
 if __name__ == '__main__':
