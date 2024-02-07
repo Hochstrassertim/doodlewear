544dd6acc85680330c00e7baeefd7e8bd030e876
Tim Hochstrasser <hochstrassert@bzz.ch>
2024-02-07 10:04:15 +0100

    Bugfix (Buttons to switch between login/register no longer submit the form, width of inputs is no longer broken)

--- app.py
+++ app.py
@@ -20,8 +20,24 @@ def login_home() -> str:
     return render_template("profile/index.html")
 
 
+@app.route('/login/submit', methods=['POST'])
+def login_submit() -> str:
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    street = request.form.get("street")
+    city = request.form.get("city")
+    postal_code = request.form.get("postal_code")
+    password = request.form.get("password")
+    birth_date = request.form.get("birth_date")
+
+    print('Hello')
+    return f'{first_name}\n{last_name}\n{email}\n{phone}\n{street}\n{city}\n{postal_code}\n{password}\n{birth_date}'
+
 # @app.route("/submit", methods=["POST"])
 # def login_submit() -> str:
+
 
 
 if __name__ == '__main__':
