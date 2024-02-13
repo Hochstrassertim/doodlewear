688fc62e83a958a9b8dc8cdc8f8ee92f6868b3c9
ia23a-schafrothl <schafrothl@bzz.ch>
2024-02-13 16:30:07 +0100

    productpage @app.route connection done

--- app.py
+++ app.py
@@ -94,6 +94,9 @@ def logout():
 @app.route('/shirts/italy')
 def italy():
     return render_template('shirts/italien.html')
+@app.route('/shirts/productpage', methods=['GET'])
+def productpage():
+    return render_template('shirts/productpage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
