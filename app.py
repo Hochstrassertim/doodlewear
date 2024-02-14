651415edc4adc968f31b5e8646e7530efa9f0613
Tim Hochstrasser <tim.hochstrasser@zkb.ch>
2024-02-14 11:11:41 +0100

    Added registration and after that an automatic login

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import time
+
 import psycopg2
 import socket
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, jsonify, url_for
 from flask_session import Session
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -10,10 +14,12 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+server_hostname = "srv-cn5lbkgl5elc73e7hus0-hibernate-689d6995f9-v54tm"
+
 def connect_to_database():
     # Connection string for PostgreSQL
 
-    if socket.gethostname() == "srv-cn5lbkgl5elc73e7hus0-hibernate-689d6995f9-v54tm":
+    if socket.gethostname() == server_hostname:
         connection_string = "postgres://postgresql:Mre24ol0TQsfbyMY7AmiHxFaiMW5qTZ2@dpg-cn52h90l6cac73a8vvu0-a/doodlewear"
     else:
         connection_string = "dbname=doodlewear user=postgresql password=Mre24ol0TQsfbyMY7AmiHxFaiMW5qTZ2 host=dpg-cn52h90l6cac73a8vvu0-a.frankfurt-postgres.render.com port=5432"
@@ -36,6 +42,32 @@ def get_user_info(cursor, username):
         return [dict(zip(columns, row)) for row in results]
     else:
         return None
+
+
+def register_user(cursor, username, first_name, last_name, email, phone, street, house_number, city, postal_code, country, password, birth_date):
+    # Check if the username already exists (case-insensitive)
+    check_query = "SELECT * FROM users WHERE LOWER(username) = LOWER(%s)"
+    cursor.execute(check_query, (username.lower(),))
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+        # Username already exists, handle accordingly (e.g., raise an exception or return an error)
+        raise ValueError("Username already exists")
+
+    # Convert birth_date to a date object
+    birth_date_obj = datetime.strptime(birth_date, '%Y-%m-%d').date()
+
+    # Insert the new user
+    insert_query = """
+        INSERT INTO users (
+            username, first_name, last_name, email, phone_number, street, house_number, city, postal_code, country, password, birth_date, role
+        ) VALUES (
+            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 'user'
+        )
+    """
+    cursor.execute(insert_query, (
+        username.lower(), first_name, last_name, email, phone, street, house_number, city, postal_code, country, password, birth_date_obj
+    ))
 
 @app.route('/')
 def hello_world():
@@ -77,12 +109,47 @@ def login():
 
     return render_template("profile/index.html")
 
-@app.route('/login_submit', methods=['POST'])
-def login_submit():
-    if request.method == 'POST':
-        session["name"] = request.form.get("name")
-        return redirect("/login")
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        street_house_number = request.form.get("street")
+        city = request.form.get("city")
+        postal_code = request.form.get("postal_code")
+        password = request.form.get("password")
+        birth_date = request.form.get("birth_date")
+
+        address = street_house_number.split(" ")
+        street = address[0]
+        house_number = address[1]
+
+        try:
+            connection = connect_to_database()
+            cursor = create_cursor(connection)
+
+            # Call the register_user function with case-insensitive username
+            register_user(cursor, username, first_name, last_name, email, phone, street, house_number, city,
+                          postal_code, 'switzerland', password, birth_date)
+            connection.commit()
+            cursor.close()
+            connection.close()
+
+            # Set the session for the newly registered user
+            session["name"] = username
+
+            response_data = {'success': True, 'message': 'Registration successful. You will be redirected in a few seconds.'}
+            time.sleep(3)
+            return render_template("profile/login_redirect.html", response_data=response_data)
+
+        except Exception as e:
+            return jsonify({'success': False, 'message': 'Error registering user: {}'.format(str(e))})
+
     return render_template("profile/index.html")
+
 
 @app.route("/profile")
 def profile():
@@ -104,6 +171,20 @@ def italy():
 @app.route('/hostname')
 def hostname():
     return socket.gethostname()
+
+@app.route('/test_register')
+def test_register():
+    try:
+        connection = connect_to_database()
+        cursor = create_cursor(connection)
+        register_user(cursor, 'tim', 'tim', 'hochstrasser', 'email', 'phone', 'street', 5, 'city', 'postal_code','switzerland', 'password', '2000-01-01')
+        connection.commit()  # Commit changes to the database
+        return "Test"
+    except Exception as e:
+        return f"Error: {str(e)}"
+    finally:
+        cursor.close()
+        connection.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/assets/js/login.js
+++ static/assets/js/login.js
@@ -1,7 +1,8 @@
 function hideFields() {
-            console.log("Test");
             document.getElementById("first_name").style.display = "none";
             document.getElementById("last_name").style.display = "none";
+            document.getElementById("email").style.display = "none";
+            document.getElementById("phone").style.display = "none";
             document.getElementById("street").style.display = "none";
             document.getElementById("city").style.display = "none";
             document.getElementById("postal_code").style.display = "none";
@@ -10,11 +11,14 @@ function hideFields() {
             document.getElementById("register").style.display = "none";
             document.getElementById("register_button").style.display = "inline-block"; // Show the Register button
             document.getElementById("login_button").style.display = "none"; // Hide the Register button
+            document.getElementById("form").action = "/login"
         }
 
         function showAllFields() {
             document.getElementById("first_name").style.display = "inline-block";
             document.getElementById("last_name").style.display = "inline-block";
+            document.getElementById("email").style.display = "inline-block";
+            document.getElementById("phone").style.display = "inline-block";
             document.getElementById("street").style.display = "inline-block";
             document.getElementById("city").style.display = "inline-block";
             document.getElementById("postal_code").style.display = "inline-block";
@@ -22,5 +26,6 @@ function hideFields() {
             document.getElementById("login").style.display = "none";
             document.getElementById("register").style.display = "inline-block";
             document.getElementById("login_button").style.display = "inline-block"; // Show the Register button
-            document.getElementById("register_button").style.display = "none"; // Hide the Register button
+            document.getElementById("register_button").style.display = "none"
+            document.getElementById("form").action = "/register"
         }
