448030070b69da4ca6acac5ee8854b5232644ba4
ia23a-perril <perril@bzz.ch>
2024-02-15 10:37:49 +0100

    Made Login perfect

--- static/assets/js/login.js
+++ static/assets/js/login.js
@@ -6,10 +6,17 @@ function register() {
     x.style.left = "-400px";
     y.style.left = "50px";
     z.style.left = "110px";
+
+    document.getElementById("login_button").style.color = "#999";
+    document.getElementById("register_button").style.color = "black";
+
 }
 
 function login() {
     x.style.left = "50px";
     y.style.left = "450px";
     z.style.left = "0px";
+
+    document.getElementById("login_button").style.color = "black";
+    document.getElementById("register_button").style.color = "#999";
 }
